2a4671fa0710c64d9c0b0c8ce0a1fcb6ea12dc37
Mihir Narkar <Mihirnarkar19@gmail.com>
2023-09-29 14:13:01 +0530

    Using Hibernate

--- src/main/java/in/mindcraft/auth2/LoginController.java
+++ src/main/java/in/mindcraft/auth2/LoginController.java
@@ -1,21 +1,33 @@
 package in.mindcraft.auth2;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
     @Autowired
-    private UserService userService;
+    private JdbcTemplate jdbcTemplate;
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
-        if (userService.checkUserCredentials(user.getUsername(), user.getPassword())) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        // Use jdbcTemplate to execute the SQL query
+        // You should also use password hashing for security
+        // For this example, I'm using plaintext passwords for simplicity
+        
+        List<Map<String,Object>> result = jdbcTemplate.queryForList(sql, user.getUsername(), user.getPassword());
+
+        if (!result.isEmpty()) {
             return "Login successful";
         } else {
             return "Login failed. Invalid username or password.";
         }
     }
 }
-
--- src/main/java/in/mindcraft/auth2/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package in.mindcraft.auth2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
-
